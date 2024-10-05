c6ee2cbc0ada84200764bd274c07f37899d6ffca
Nelson Salgado <nsalgado@tecniseguros.com>
2024-10-04 20:33:57 -0600

    End of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,15 @@
-using System;
-using System.Diagnostics;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")]
-
-public class UsersController(DataContext context) : ControllerBase   
+[Authorize]
+public class UsersController(DataContext context) : BaseApiController   
 {   
-    
+    [AllowAnonymous]
 
     [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
@@ -21,7 +18,7 @@ public class UsersController(DataContext context) : ControllerBase
     
             return users;
     }
-
+    [Authorize]
     [HttpGet("{id}")] // api/users/3
         public async Task<ActionResult<AppUser>> GetUser(int id){
         
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace API.Entities;
 
 public class AppUser
@@ -7,6 +5,8 @@ public class AppUser
     public int Id { get; set; }
     public required string UserName { get; set; } 
 
+    public required byte[] PasswordHash { get; set; } 
+    public required byte[] PasswordSalt { get; set; } 
     
 
 
